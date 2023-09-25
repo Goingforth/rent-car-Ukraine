45ba5fcc247d4368ff36dff0e393eb5e538d4c8f
Oleksandr Siukov <as-service2008@ukr.net>
2023-09-25 22:32:44 +0300

    clear extra files

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App/App';
 import { BrowserRouter } from "react-router-dom";
-import reportWebVitals from './reportWebVitals';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -14,4 +14,4 @@ root.render(
 
   // </React.StrictMode>
 );
-reportWebVitals();
+
