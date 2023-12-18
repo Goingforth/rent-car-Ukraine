9a639541a0a3616f8672376315ef9542f30c5737
Oleksandr Siukov <as-service2008@ukr.net>
2023-12-18 18:48:01 +0200

    find eror netify

--- src/index.js
+++ src/index.js
@@ -8,8 +8,8 @@ import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
-  <BrowserRouter basename="/rent-car-Ukraine">
-    {/* <BrowserRouter > */}
+  // <BrowserRouter basename="/rent-car-Ukraine">
+  <BrowserRouter >
     <App />
   </BrowserRouter>
 
